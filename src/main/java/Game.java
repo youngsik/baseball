59382871234b8a5e85329be1cf9392e6e37c13d1
dean <dean4208@gmail.com>
2025-06-04 13:53:20 +0900

    [feature] 39p

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -15,5 +15,12 @@ public class Game {
                 throw new IllegalArgumentException();
             }
         }
+
+        if(guessNumber.charAt(0) ==guessNumber.charAt(1)
+        ||guessNumber.charAt(0) ==guessNumber.charAt(2)
+        ||guessNumber.charAt(1) ==guessNumber.charAt(2)){
+            throw new IllegalArgumentException();
+        }
+
     }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -25,6 +25,7 @@ class GameTest {
         assertIllegalArgument("12");
         assertIllegalArgument("1234");
         assertIllegalArgument("12s");
+        assertIllegalArgument("121");
     }
 
     private void assertIllegalArgument(String guessNumber) {
