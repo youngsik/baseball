a53cbf90f9a2fca3819ac13e67cde1a606ed0481
dean <dean4208@gmail.com>
2025-06-04 13:40:13 +0900

    [feature] 28p

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -25,4 +25,12 @@ class GameTest {
             game.guess(null);
         });
     }
+
+    @Test
+    void throwExceptionWhenInputLengthIsUnmatched() {
+        assertThrows(IllegalArgumentException.class, ()->{
+            game.guess("12");
+        });
+    }
+
 }
