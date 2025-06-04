a1d93c57e0c9503efcb1904f03a2a8d27a33a9f9
dean <dean4208@gmail.com>
2025-06-04 13:55:14 +0900

    [refactoring] 41p

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,26 +1,32 @@
 public class Game {
     public void guess(String guessNumber) {
-        if(guessNumber==null)
-        {
+        assertIlleagalArgument(guessNumber);
+
+    }
+
+    private static void assertIlleagalArgument(String guessNumber) {
+        if (guessNumber == null) {
             throw new IllegalArgumentException();
         }
 
-        if(guessNumber.length() != 3)
-        {
+        if (guessNumber.length() != 3) {
             throw new IllegalArgumentException();
         }
 
-        for(char number : guessNumber.toCharArray()){
-            if(number<'0'||number>'9'){
+        for (char number : guessNumber.toCharArray()) {
+            if (number < '0' || number > '9') {
                 throw new IllegalArgumentException();
             }
         }
 
-        if(guessNumber.charAt(0) ==guessNumber.charAt(1)
-        ||guessNumber.charAt(0) ==guessNumber.charAt(2)
-        ||guessNumber.charAt(1) ==guessNumber.charAt(2)){
+        if (isDuplicatedNumber(guessNumber)) {
             throw new IllegalArgumentException();
         }
+    }
 
+    private static boolean isDuplicatedNumber(String guessNumber) {
+        return guessNumber.charAt(0) == guessNumber.charAt(1)
+                || guessNumber.charAt(0) == guessNumber.charAt(2)
+                || guessNumber.charAt(1) == guessNumber.charAt(2);
     }
 }
